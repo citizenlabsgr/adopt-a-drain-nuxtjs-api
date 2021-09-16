f9b51024562996369c4fb0b621951024bc569dae
James <wilfongjt@gmail.com>
2021-09-16 06:10:23 -0400

    stop.allocating.connection.when.not.required

---
+++ scripts/dk.install.sh
@@ -0,0 +1,9 @@
+#scripts/dk.build.sh
+cd ..
+
+npm install 
+
+docker images 
+# docker run ls IMAGE citizenlabsgr/adopt-a-drain-hapi-api
+# docker exec -t -i citizenlabsgr/adopt-a-drain-hapi-api sh
+# docker image inspect citizenlabsgr/adopt-a-drain-hapi-api
