a2035dfe1446a9261bf6a4798d1a7693d2e6e887
James <wilfongjt@gmail.com>
2021-10-19 05:44:50 -0400

    08_adopter

--- models/db/drop_functions.js
+++ models/db/drop_functions.js
@@ -8,6 +8,8 @@ module.exports = class DropFunction extends Step {
     // [Function drops occur when a function's name or parameters change]
     
     this.sql = `
+    DROP FUNCTION if exists api_0_0_1.signup(TEXT,JSON,TEXT); 
+    DROP FUNCTION if exists api_0_0_1.signin(TEXT,JSON); 
     DROP FUNCTION if exists api_0_0_1.adopter(TEXT,JSON); 
     `;
   }    
