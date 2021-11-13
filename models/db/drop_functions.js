c4766d8ee8d55bc24d2117eb20a59c067d0881ec
James <wilfongjt@gmail.com>
2021-11-13 13:22:58 -0500

    09_adoptee

--- models/db/drop_functions.js
+++ models/db/drop_functions.js
@@ -8,10 +8,10 @@ module.exports = class DropFunction extends Step {
     // [Function drops occur when a function's name or parameters change]
     
     this.sql = `
-    -- DROP TYPE if exists owner_id; issue with depenency
+    
     DROP FUNCTION if exists base_0_0_1.query(mbr);
 
-    DROP FUNCTION if exists function api_0_0_1.adoptee(token, jsonb) ;
+    -- DROP FUNCTION if exists function api_0_0_1.adoptee(token, jsonb) ;
     
     DROP FUNCTION if exists api_0_0_1.adoptee(TOKEN,JSON);
 
